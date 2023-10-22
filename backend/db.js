142231ee31431ba50fe11b6b033f449c885c3eaf
juantoledo1 <juanmanueltoledo0487@gmail.com>
2023-10-22 15:58:37 -0300

    ramas funcionando sin conflictos

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise'); // Usamos mysql2/promise para la conexi
 const dbConfig = {
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: 'Sinley123.',
   database: 'bd_fenix',
 };
 
